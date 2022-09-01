c1ddffdb01f71cd8bcc53fe38c4267c4fcf79011
snoskova07 <snoskova07@gmail.com>
2022-09-02 01:18:18 +0700

    Change the test to page object style. Almost.

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,10 @@
 import time
-from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser, user_language):
     link = f"http://selenium1py.pythonanywhere.com/{user_language}"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-    time.sleep(5)
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()
+    time.sleep(2)
