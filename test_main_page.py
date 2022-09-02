e08ee257d1f6880a89476c7a10e8c63e27dfe8d0
snoskova07 <snoskova07@gmail.com>
2022-09-02 15:51:24 +0700

    add try/except for catching NoSuchElementException

--- test_main_page.py
+++ test_main_page.py
@@ -4,7 +4,14 @@ from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser, user_language):
     link = f"http://selenium1py.pythonanywhere.com/{user_language}"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
     page.go_to_login_page()
     time.sleep(2)
+
+
+def test_guest_should_see_login_link(browser, user_language):
+    link = f"http://selenium1py.pythonanywhere.com/{user_language}"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
