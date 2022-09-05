5baa6d36c28ba99a411d779624ef516febff5073
snoskova07 <snoskova07@gmail.com>
2022-09-05 13:38:10 +0700

    finish 4.3

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 import time
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser, user_language):
@@ -7,7 +8,9 @@ def test_guest_can_go_to_login_page(browser, user_language):
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()
-    time.sleep(2)
+    login_page = LoginPage(browser, link)
+    login_page.should_be_login_page()
+    time.sleep(3)
 
 
 def test_guest_should_see_login_link(browser, user_language):
